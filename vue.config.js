08d1f3023bb0954f3e0a00809a123db49beec8b0
Jacob Parra <jacobisraelparra@gmail.com>
2021-04-15 21:14:06 -0700

    Testing new theme

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production'
     ? '/sungineers.github.io'
-    : '/'
+    : '/',
+  css: {
+    loaderOptions: {
+      css: {
+        sourceMap: process.env.NODE_ENV !== "production" ? true : false
+      }
+    }
   }
+};
